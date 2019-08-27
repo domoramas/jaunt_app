ef1805095230edc307ab92ba07d61e520a43579b
domoramas <boombappro@gmail.com>
2019-08-27 12:10:51 -0700

    added postgres db

--- jaunt_project/jaunt/settings.py
+++ jaunt_project/jaunt/settings.py
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'jaunt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jaunt_db',
+        'USER': 'jaunt',
+        'PASSWORD': 'jaunt1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 #  change database to postgres  
